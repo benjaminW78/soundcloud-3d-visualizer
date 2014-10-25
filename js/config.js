663a1e82ce477448fb4bcb2d089ee862c1bd5cbd
ben <ben@ben-linux.(none)>
2014-10-25 22:40:23 +0200

    add of plane render on  wireframe with with vertex modification

--- js/config.js
+++ js/config.js
@@ -3,3 +3,4 @@
     * *******************************************************************/
     var particleCount = 1000;
     var particleSize = 30;
+     window.location+="#daisuke-sasaki-3/bee-gees-how-deep-is-your-love";
